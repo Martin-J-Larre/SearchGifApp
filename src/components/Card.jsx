4b2aabb69831a3ee2a0701fe2ef03eccc2c5999d
Martin-J-Larre <martinjlarre@gmail.com>
2023-03-31 23:57:06 -0400

    #4 Add barrel to components folder

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Card = ({ title, url, id }) => {
   return (
     <>
-      <div>
+      <div className="card">
         <img src={url} alt={title} />
         <p>{title}</p>
       </div>
